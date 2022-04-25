dd9bd3e294106f495f698e72324f9b2bc645f9e2
Jeffersson Galvao <jefferssondeveloper@gmail.com>
2022-04-25 20:12:11 -0300

    Criação da tabela de seguidores

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS social_network;
 USE social_network;
 
-DROP WITH EXISTS users;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -10,4 +11,19 @@ CREATE TABLE users(
     email varchar(255) not null unique,
     password varchar(255) not null,
     created_at timestamp default current_timestamp()
+) ENGINE=INNODB;
+
+
+CREATE TABLE followers (
+    user_id int not null,
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    follower_id int not null,
+    FOREIGN KEY (follower_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    primary key(user_id, follower_id)
 ) ENGINE=INNODB;
